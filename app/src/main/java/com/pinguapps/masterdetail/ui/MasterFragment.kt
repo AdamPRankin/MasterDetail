2222ef3cbee4e0ba28157a3602881ba4d5c73058
adam <arankin@ualberta.ca>
2022-12-19 14:42:41 -0700

    initial commit

--- app/src/main/java/com/pinguapps/masterdetail/ui/MasterFragment.kt
+++ app/src/main/java/com/pinguapps/masterdetail/ui/MasterFragment.kt
@@ -1,26 +1,19 @@
 package com.pinguapps.masterdetail.ui
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.viewModels
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.pinguapps.masterdetail.R
 import com.pinguapps.masterdetail.util.isTablet
-import androidx.appcompat.app.AppCompatActivity
-
 
 class MasterFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = MasterFragment()
-    }
-    val masterViewModel: MasterViewModel by activityViewModels()
+    
+    private val masterViewModel: MasterViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,7 +24,6 @@ class MasterFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -39,33 +31,16 @@ class MasterFragment : Fragment() {
         val masterRecyclerAdapter = MasterRecyclerAdapter {
             masterViewModel.selectCurrentModelObject(it)
             if (!isTablet(requireContext())){
-                //todo new fragment
+                parentFragmentManager
+                    .beginTransaction()
+                    .add(R.id.container, DetailFragment())
+                    .hide(this)
+                    .commit()
             }
-
         }
         val masterLinearLayoutManager = LinearLayoutManager(requireContext())
         masterRecyclerView.adapter = masterRecyclerAdapter
         masterRecyclerView.layoutManager = masterLinearLayoutManager
         super.onViewCreated(view, savedInstanceState)
-    }
-
-    override fun onStart() {
-        super.onStart()
-    }
-
-    override fun onResume() {
-        super.onResume()
-    }
-
-    override fun onPause() {
-        super.onPause()
-    }
-
-    override fun onStop() {
-        super.onStop()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
     }
 }
